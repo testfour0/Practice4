c6413f5109b376fdba8283fd032d50461fe797f1
Esteban <esteban31693@gmail.com>
2024-06-25 10:59:25 -0400

    Working with uploaded CSV file. Limit increased to 5 Columns. Added button to homepage so you dont have to manually go to /upload. Added functionality to hide columns, and to filter contents per column. Also have working reset button for those values(if you have no columns selected it errors out lol. Also is case-sensitive). Added background, banner, and more appearance stuff to everything. -> added login page. It tracks login attempts, but I can't get it to work with the Database as far as authenticating. -> DB set up. I have added a few users in data.sql and 2 admins. Have to use these credentials to access the upload page.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- Insert initial data into the users table
+INSERT INTO users (username, password, role) VALUES ('admin', 'admin123', 'ROLE_ADMIN');
+INSERT INTO users (username, password, role) VALUES ('peter', 'pigeon', 'ROLE_USER');
+INSERT INTO users (username, password, role) VALUES ('admin2', 'admin1234', 'ROLE_ADMIN');
+INSERT INTO users (username, password, role) VALUES ('adamn', 'levine', 'ROLE_USER');
+INSERT INTO users (username, password, role) VALUES ('carlos', 'sainz', 'ROLE_USER');
+INSERT INTO users (username, password, role) VALUES ('daniel', 'riccardo', 'ROLE_USER');
